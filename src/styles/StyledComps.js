c5adc395219bbd6bad21aa54e1f11b4cd7edf7d1
Lucky-BE <byungeun96@gmail.com>
2024-02-29 15:36:18 +0900

    Rename: 폴더명 수정

---
+++ src/styles/StyledComps.js
@@ -0,0 +1,21 @@
+/**
+ * @format
+ **/
+
+import { Col } from "antd";
+import styled from "styled-components";
+
+export const ColTitle = styled(Col)`
+  background-color: rgb(242 242 242);
+  text-align: center;
+  border: 1px solid #dfdfdf;
+  height: 32px;
+  line-height: 2em;
+`;
+
+export const ColData = styled(Col)`
+  border: 1px solid #dfdfdf;
+  height: 32px;
+  padding-left: 10px;
+  line-height: 2em;
+`;
